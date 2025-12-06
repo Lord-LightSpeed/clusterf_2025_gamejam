8a879b1aee9c231e7790acc9b14223833431e3a7
Lord-LightSpeed <adamkyte1@gmail.com>
2025-12-06 19:10:37 +0000

    bullets now hit the player... this is pain

--- Assets/scripts/shooter.cs
+++ Assets/scripts/shooter.cs
@@ -1,7 +1,9 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class shooter : MonoBehaviour
 {
+    public Camera camera;
     public GameObject bullet;
     public Transform turret;
     public GameObject player;
@@ -16,6 +18,8 @@ public class shooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 vecToMouse = camera.ScreenToWorldPoint(Input.mousePosition + new Vector3(0,0,10)) - playerloc.position;
+        turret.rotation = quaternion.Euler(0, 0, math.atan2(vecToMouse.y, vecToMouse.x));
         if (Input.GetMouseButtonDown(0))
         {
             Instantiate(bullet, playerloc.position, turret.rotation);
