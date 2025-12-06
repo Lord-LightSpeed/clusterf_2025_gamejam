9f2df4c726c31abc333fa40c7c505bede4ae53f8
Lord-LightSpeed <adamkyte1@gmail.com>
2025-12-06 18:00:22 +0000

    it works, idk why, the name of this project is accurate

--- Assets/scripts/shooter.cs
+++ Assets/scripts/shooter.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class shooter : MonoBehaviour
 {
     public GameObject bullet;
-    private Rigidbody2D turret;
+    public Transform turret;
+    public GameObject player;
+    private Transform playerloc;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        turret = GetComponent<Rigidbody2D>();
+        playerloc = player.GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -15,7 +18,7 @@ public class shooter : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(bullet, turret.position, Quaternion.Euler(0, 0, turret.rotation));
+            Instantiate(bullet, playerloc.position, turret.rotation);
         }
     }
 }
