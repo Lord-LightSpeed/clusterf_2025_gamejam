a00e6058630a28225eeee045c238229a9ccefa78
Lord-LightSpeed <adamkyte1@gmail.com>
2025-12-06 16:05:05 +0000

    I hate unity

---
+++ Assets/scripts/shooter.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class shooter : MonoBehaviour
+{
+    public GameObject bullet;
+    private Rigidbody2D turret;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        turret = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Instantiate(bullet, turret.position, Quaternion.Euler(0, 0, turret.rotation));
+        }
+    }
+}
