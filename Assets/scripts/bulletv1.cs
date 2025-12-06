012e528cb5bd80a47190366a1ac3f554c74227ca
Lord-LightSpeed <adamkyte1@gmail.com>
2025-12-06 19:28:14 +0000

    we shoot at 90°, fuck it, we ball!

---
+++ Assets/scripts/bulletv1.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class bulletv1 : MonoBehaviour
+{
+    private GameObject self;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.linearVelocity = transform.up * 10f;
+        self = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy") || collision.CompareTag("boundary"))
+        {
+            Destroy(collision);
+            Destroy(self);
+        }
+    }
+}
