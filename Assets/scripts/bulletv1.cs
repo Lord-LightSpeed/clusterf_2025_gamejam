c75e936dc894661f58a99d786801bfca339f265c
Lord-LightSpeed <adamkyte1@gmail.com>
2025-12-07 08:26:29 +0000

    ehh, bullets just off themselves 30 secs after leaving screen

--- Assets/scripts/bulletv1.cs
+++ Assets/scripts/bulletv1.cs
@@ -1,24 +1,41 @@
+using System.Data.Common;
+using System.Threading;
 using UnityEngine;
 
 public class bulletv1 : MonoBehaviour
 {
-    private GameObject self;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private double time_to_die;
     void Start()
     {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.linearVelocity = transform.up * 10f;
-        self = GetComponent<GameObject>();
-        Destroy(self);
+        time_to_die = double.MaxValue;
+    }
+
+    void Update()
+    {
+        if (Time.timeSinceLevelLoadAsDouble >= time_to_die)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.CompareTag("Enemy") || collision.CompareTag("boundary"))
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("boundary"))
         {
-            Destroy(collision);
-            Destroy(self);
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
         }
+    }
+    void OnBecameInvisible()
+    {
+        time_to_die = Time.timeSinceLevelLoadAsDouble + 30d;
+    }
+    void OnBecameVisible()
+    {
+        time_to_die = double.MaxValue;
     }
 }
